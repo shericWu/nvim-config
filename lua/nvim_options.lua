ddca4f07ce6f3143c1905b7115f7c5bd177532d4
shericWu <shihyu1127@gmail.com>
2024-02-18 23:21:58 +0800

    add relative line number, add keymap for buffers/tabs

--- lua/nvim_options.lua
+++ lua/nvim_options.lua
@@ -8,6 +8,7 @@ vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = "number"
+vim.opt.relativenumber = true
 
 -- set leader key to space
 vim.g.mapleader = " "
@@ -19,4 +20,13 @@ vim.opt.signcolumn = "number" -- or "yes:4"
 vim.opt.timeoutlen = 300
 
 -- set cursor style
-vim.opt.guicursor="n-c:block,i-ci-ve:ver25-blinkwait700-blinkoff400-blinkon250,r-cr-o-v-sm:hor20"
+vim.opt.guicursor = "n-c:block,i-ci-ve:ver25-blinkwait700-blinkoff400-blinkon250,r-cr-o-v-sm:hor20"
+
+-- ask when closing file with unsaved changes
+vim.opt.confirm = true
+
+-- keymap
+vim.keymap.set("n", "<C-b>n", "<cmd>bn<CR>", {})
+vim.keymap.set("n", "<C-b>p", "<cmd>bN<CR>", {})
+vim.keymap.set("n", "<C-t>n", "<cmd>tabnew<CR>", {})
+vim.keymap.set("n", "<C-t>q", "<cmd>tabclose<CR>", {})

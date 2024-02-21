e6b5d72e80c101cd91a0fc0e37d655f6900299e2
shericWu <shihyu1127@gmail.com>
2024-02-21 11:35:43 +0800

    add [$][`] autoclose for markdown

--- lua/plugins/autoclose.lua
+++ lua/plugins/autoclose.lua
@@ -1,7 +1,30 @@
 return {
-	"m4xshen/autoclose.nvim",
-    
-	config = function()
-		require("autoclose").setup()
-	end,
+    "m4xshen/autoclose.nvim",
+
+    config = function()
+        require("autoclose").setup({
+            keys = {
+                ["$"] = {
+                    close = true,
+                    escape = false,
+                    pair = "$$",
+                    disabled_filetypes = {},
+                    enabled_filetypes = {
+                        "markdown",
+                    },
+                    disable_command_mode = true,
+                },
+                ["`"] = {
+                    close = true,
+                    escape = false,
+                    pair = "``",
+                    disabled_filetypes = {},
+                    enabled_filetypes = {
+                        "markdown",
+                    },
+                    disable_command_mode = true,
+                },
+            },
+        })
+    end,
 }

a585573e6518aab40553223e67cadd972330b1db
shericWu <shihyu1127@gmail.com>
2024-03-11 09:09:06 +0800

    add some plug in to catppuccin

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,14 +1,25 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
 
-    config = function()
-        local catppuccin = require("catppuccin")
-        catppuccin.setup({
-            transparent_background = true,
-        })
-        vim.cmd.colorscheme("catppuccin")
-        vim.cmd.highlight({ "LineNr", "guifg=#6c83ac" })
-    end,
+	config = function()
+		local catppuccin = require("catppuccin")
+		catppuccin.setup({
+			transparent_background = true,
+			integrations = {
+				telescope = {
+					enabled = true,
+				},
+				notify = true,
+                dap = true,
+                cmp = true,
+                noice = false,
+                mason = false,
+                neotree = false,
+			},
+		})
+		vim.cmd.colorscheme("catppuccin")
+		vim.cmd.highlight({ "LineNr", "guifg=#6c83ac" })
+	end,
 }

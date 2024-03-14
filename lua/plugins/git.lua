ded3a614792fbcf0f326a1c8805e18a37ffce34b
shericWu <shihyu1127@gmail.com>
2024-03-14 12:55:55 +0800

    added trailing space

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -15,7 +15,7 @@ return {
 
 		config = function()
 			require("gitsigns").setup()
-			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+			vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk_inline<CR>", {})
 		end,
 	},
     {
